feb89f216487b45542750b9fa9d1e3e8c2ededa9
dumpalamano <46875335+dumpalamano@users.noreply.github.com>
2026-02-21 20:02:36 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'fatburn-v3';
+const BASE = '/fat-burn-pwa-dumpala/';
+const ASSETS = [BASE, BASE+'index.html', BASE+'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(()=>c)));
+});
